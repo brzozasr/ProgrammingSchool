edcf46575145fe3c4b73d041f8ecc42dba2ae7e6
Sławomir Brzozowski <brzozasr@interia.eu>
2021-05-29 09:46:24 +0200

    Init commit.

---
+++ ProgrammingSchool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgrammingSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
